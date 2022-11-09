ce3d06759ce509b7d87e220d3f84d0161921b6b1
Subham Rai <93194290+Subham-e@users.noreply.github.com>
2022-11-10 01:19:51 +0530

    Added keyboard eventlistener

--- index.js
+++ index.js
@@ -2,47 +2,69 @@ var buttonLength = document.querySelectorAll("button").length; //length of all 7
 
 
 for(var i = 0; i<buttonLength; i++){ //loop to select all buttons
-document.querySelectorAll("button")[i].addEventListener("click", function(){
-var buttonInnerHTML = this.innerHTML; //finding the clicked button
-switch (buttonInnerHTML) {
-  case "w":
-    var tom1 = new Audio("sounds/tom-1.mp3");
-    tom1.play();
-    break;
 
-  case "a":
-    var tom2 = new Audio("sounds/tom-2.mp3");
-    tom2.play();
-    break;
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
 
-  case "s":
-    var tom3 = new Audio("sounds/tom-3.mp3");
-    tom3.play();
-    break;
+      var buttonInnerHTML = this.innerHTML; //finding the clicked button
+      makeSound(buttonInnerHTML);
+      animation(buttonInnerHTML);
+      });
 
-  case "d":
-    var tom4 = new Audio("sounds/tom-4.mp3");
-    tom4.play();
-    break;
+  document.addEventListener("keypress", function(e){//finding the keyboard button pressed
+     makeSound(e.key);
+     animation(e.key);
+     });
+};//loop ends here
 
- case "j":
-    var snare= new Audio("sounds/snare.mp3");
-    snare.play();
-    break;
+function makeSound(key){//function to swtich between key pressed to play Audio accordingly
 
- case "k":
-    var crash = new Audio("sounds/crash.mp3");
-    crash.play();
-    break;
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
 
-case "l":
-    var kick = new Audio("sounds/kick-bass.mp3");
-    kick.play();
-    break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
 
-  default:
-  console.log(this.innerHTML);
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+   case "j":
+      var snare= new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+   case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+  case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+    console.log(this.innerHTML);
+  }
+}
+
+function animation(currentKey){
+  var activeButton = document.querySelector("." +currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
 
 }
-})
-};
